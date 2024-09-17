90f097250d018458d3485f34aadc031e5987f345
Nicolai <nicolai.martin@pcsoluciones.com.co>
2024-09-17 18:17:47 -0500

    feat: Inicio de dashboard para bares

---
+++ src/app/dashboard/(main)/Main.tsx
@@ -0,0 +1,16 @@
+import styles from './(estilos)/Main.module.css'
+
+const Main = () => {
+return (
+  <main className={styles.contenedor_main}>
+    <div>primer elemento</div>
+    <div>segundo elemento</div>
+    <div>tercer elemento</div>
+    <div>cuarto elemento</div>
+    <div>quinto elemento</div>
+    <div>sexto elemento</div>
+  </main>
+  );
+};
+
+export default Main;
